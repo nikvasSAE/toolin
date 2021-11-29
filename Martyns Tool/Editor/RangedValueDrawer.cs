fd4d8c7751752302dff4cc50129697b7365c0a17
Martyn_E <1022425@student.sae.edu.au>
2021-11-30 07:13:07 +1000

    Updated: Applied Niks feedback.

--- Martyns Tool/Editor/RangedValueDrawer.cs
+++ Martyns Tool/Editor/RangedValueDrawer.cs
@@ -1,104 +1,93 @@
-using PlasticPipe.PlasticProtocol.Messages;
 using UnityEngine;
 using UnityEditor;
 
-namespace MartynTool
+namespace Martyn
 {
     [CustomPropertyDrawer(typeof(RangedInt), true)]
     [CustomPropertyDrawer(typeof(RangedFloat), true)]
     public class RangedValueDrawer : PropertyDrawer
     {
+        private float _minValue;
+        private float _maxValue;
+        
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             label = EditorGUI.BeginProperty(position, label, property);
             position = EditorGUI.PrefixLabel(position, label);
 
-            var minProp = property.FindPropertyRelative("minValue");
-            var maxProp = property.FindPropertyRelative("maxValue");
+            SerializedProperty minProp = property.FindPropertyRelative("minValue");
+            SerializedProperty maxProp = property.FindPropertyRelative("maxValue");
 
             switch (minProp.propertyType)
             {
                 case SerializedPropertyType.Integer:
                 {
-                    float minValue = minProp.intValue;
-                    float maxValue = maxProp.intValue;
-                    const string decimals = "f0";
-                    
-                    float rangeMin = 0;
-                    float rangeMax = 1;
-                    
-                    var ranges = (MinMaxRangeAttribute[])fieldInfo.GetCustomAttributes(typeof(MinMaxRangeAttribute), true);
-                    if (ranges.Length > 0)
-                    {
-                        rangeMin = ranges[0].Min;
-                        rangeMax = ranges[0].Max;
-                    }
-                    
-                    const float rangeBoundsLabelWidth = 40f;
-                    
-                    var rangeBoundsLabel1Rect = new Rect(position)
-                    {
-                        width = rangeBoundsLabelWidth
-                    };
-                    GUI.Label(rangeBoundsLabel1Rect, new GUIContent(minValue.ToString(decimals)));
-                    position.xMin += rangeBoundsLabelWidth;
-                    
-                    var rangeBounds2Rect = new Rect(position);
-                    rangeBounds2Rect.xMin = rangeBounds2Rect.xMax - rangeBoundsLabelWidth;
-                    GUI.Label(rangeBounds2Rect, new GUIContent(maxValue.ToString(decimals)));
-                    position.xMax -= rangeBoundsLabelWidth;
-                    
-                    EditorGUI.BeginChangeCheck();
-                    EditorGUI.MinMaxSlider(position, ref minValue, ref maxValue, rangeMin, rangeMax);
-                    if (EditorGUI.EndChangeCheck())
-                    {
-                        minProp.intValue = (int)minValue;
-                        maxProp.intValue = (int)maxValue;
-                    }
-                    EditorGUI.EndProperty();
+                    SliderGUI("integer", "F0", minProp, maxProp, position);
                     break;
                 }
                 case SerializedPropertyType.Float:
                 {
-                    var minValue = minProp.floatValue;
-                    var maxValue = maxProp.floatValue;
-                    const string decimals = "f2";
-
-                    float rangeMin = 0;
-                    float rangeMax = 1;
-
-                    var ranges = (MinMaxRangeAttribute[])fieldInfo.GetCustomAttributes(typeof(MinMaxRangeAttribute), true);
-                    if (ranges.Length > 0)
-                    {
-                        rangeMin = ranges[0].Min;
-                        rangeMax = ranges[0].Max;
-                    }
-
-                    const float rangeBoundsLabelWidth = 40f;
-
-                    var rangeBoundsLabel1Rect = new Rect(position)
-                    {
-                        width = rangeBoundsLabelWidth
-                    };
-                    GUI.Label(rangeBoundsLabel1Rect, new GUIContent(minValue.ToString(decimals)));
-                    position.xMin += rangeBoundsLabelWidth;
-                    
-                    var rangeBounds2Rect = new Rect(position);
-                    rangeBounds2Rect.xMin = rangeBounds2Rect.xMax - rangeBoundsLabelWidth;
-                    GUI.Label(rangeBounds2Rect, new GUIContent(maxValue.ToString(decimals)));
-                    position.xMax -= rangeBoundsLabelWidth;
-
-                    EditorGUI.BeginChangeCheck();
-                    EditorGUI.MinMaxSlider(position, ref minValue, ref maxValue, rangeMin, rangeMax);
-                    if (EditorGUI.EndChangeCheck())
-                    {
-                        minProp.floatValue = minValue;
-                        maxProp.floatValue = maxValue;
-                    }
-                    EditorGUI.EndProperty();
+                    SliderGUI("float", "F2", minProp, maxProp, position);
                     break;
                 }
             }
+        }
+        
+        private void SliderGUI(string numType, string decimals, SerializedProperty minProp, SerializedProperty maxProp, Rect position)
+        {
+            switch (numType)
+            {
+                case "integer":
+                    _minValue = minProp.intValue;
+                    _maxValue = maxProp.intValue;
+                    break;
+                case "float":
+                    _minValue = minProp.floatValue;
+                    _maxValue = maxProp.floatValue;
+                    break;
+            }
+            
+            float rangeMin = 0;
+            float rangeMax = 1;
+
+            MinMaxRangeAttribute[] ranges = (MinMaxRangeAttribute[]) fieldInfo.GetCustomAttributes(typeof(MinMaxRangeAttribute), true);
+            if (ranges.Length > 0)
+            {
+                rangeMin = ranges[0].Min;
+                rangeMax = ranges[0].Max;
+            }
+
+            const float rangeBoundsLabelWidth = 40f;
+
+            Rect rangeBoundsLabel1Rect = new Rect(position)
+            {
+                width = rangeBoundsLabelWidth
+            };
+            GUI.Label(rangeBoundsLabel1Rect, new GUIContent(_minValue.ToString(decimals)));
+            position.xMin += rangeBoundsLabelWidth;
+
+            Rect rangeBounds2Rect = new Rect(position);
+            rangeBounds2Rect.xMin = rangeBounds2Rect.xMax - rangeBoundsLabelWidth;
+            GUI.Label(rangeBounds2Rect, new GUIContent(_maxValue.ToString(decimals)));
+            position.xMax -= rangeBoundsLabelWidth;
+
+            EditorGUI.BeginChangeCheck();
+            EditorGUI.MinMaxSlider(position, ref _minValue, ref _maxValue, rangeMin, rangeMax);
+            if (EditorGUI.EndChangeCheck())
+            {
+                switch (numType)
+                {
+                    case "integer":
+                        minProp.intValue = (int) _minValue;
+                        maxProp.intValue = (int) _maxValue;
+                        break;
+                    case "float":
+                        minProp.floatValue = _minValue;
+                        maxProp.floatValue = _maxValue;
+                        break;
+                }
+            }
+            EditorGUI.EndProperty();
         }
     }
 }
